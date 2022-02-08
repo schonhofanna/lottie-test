974c652387630bb3c3316b9f9eed135927910ebe
Anna Schönhof <schonhofanna@gmail.com>
2022-02-08 13:00:15 +0100

    init

---
+++ script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets7.lottiefiles.com/packages/lf20_joql7wms.json' // lottie file path
+  })
